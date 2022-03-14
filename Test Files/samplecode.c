d6088462c53678e0a5d0e5a4a0c184bef516098d
G. Aswin <aswin.g.rns@gmail.com>
2022-03-14 16:16:21 +0530

    engine push

--- Test Files/samplecode.c
+++ Test Files/samplecode.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    char msg[50];
-    scanf("%s", msg);
+    // char msg[50];
+    // scanf("%s", msg);
 
-    printf("Message was %s\n", msg);
+    printf("Message was %s\n", "hello");
     int *p = (int*)malloc(sizeof(int));
     return 0;
 }
